08bfcffb59f87fe0f7a1e2618c3dd09d969a9140
Abdushakhidov Abdullo <147158343+Abdushahidov07@users.noreply.github.com>
2025-07-23 19:20:47 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ with col1:
 with col2:
   fig2 = px.scatter(df, x='bill_length_mm', y="flipper_length_mm",color="species", title="Длина клюва vs Длина крыла")
   st.plotly_chart(fig2, use_container_width=True)
+
+
+X=df.drop(['species'], axis=1)
+y=df['species']
+X_trin, X_test, y_train, y_test = train_test_split(X, y, test_sixe=0.3, random_state=42)
